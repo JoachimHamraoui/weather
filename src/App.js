bea9377fa0211d84bf5934da3fa59b127861604f
JoachimHamraoui <joachimhamraoui@gmail.com>
2025-04-07 21:21:52 +0200

    feat(UI): base UI info (fonts and tailwindcss) have been integrated

--- src/App.js
+++ src/App.js
@@ -3,21 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-full h-screen bg-gray-100 flex justify-center'> 
+      <div className='flex justify-center flex-col'>
+      <h1 className='text-4xl font-bold text-montserrat text-gray-800 mb-4'>Weather_</h1>
+      <div className="bg-blue-600 h-[720px] w-[600px] rounded-2xl shadow-md drop-shadow-xl">
+        
+      </div>
+    </div>
     </div>
   );
 }

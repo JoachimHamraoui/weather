32e456d7d083fa0cc9aa28701709a66e16ac53fb
JoachimHamraoui <joachimhamraoui@gmail.com>
2025-04-08 21:48:18 +0200

    feat(api): API service is now in place with environment variables

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { fetchWeather } from "./services/weatherApi";
 
 function App() {
+  const [city, setCity] = useState("");
+  const [data, setData] = useState(null);
+
+  const handleSearch = async () => {
+    try {
+      const data = await fetchWeather(city);
+      setData(data);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      handleSearch();
+      event.preventDefault();
+      console.log(data);
+    }
+  };
+  
+
+  useEffect(() => {
+
+  }, []);
   return (
-    <div className='w-full h-screen bg-gray-100 flex justify-center'> 
-      <div className='flex justify-center flex-col'>
-      <h1 className='text-4xl font-bold text-montserrat text-gray-800 mb-4'>Weather_</h1>
-      <div className="bg-blue-600 h-[720px] w-[600px] rounded-2xl shadow-md drop-shadow-xl">
-        
+    <div className="w-full h-screen bg-gray-100 flex justify-center">
+      <div className="flex justify-center flex-col">
+        <div className="w-full flex justify-between items-center">
+          <h1 className="text-4xl font-bold text-montserrat text-gray-800 mb-4">
+            Weather_
+          </h1>
+          <input
+            type="text"
+            className="border border-gray-300 rounded-md py-2 px-4 mb-4 font-montserrat"
+            placeholder="Search..."
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            onKeyDown={handleKeyDown}
+          />
+        </div>
+        <div className="bg-blue-600 h-[720px] w-[600px] rounded-2xl shadow-md drop-shadow-xl"></div>
       </div>
-    </div>
     </div>
   );
 }
---
+++ src/services/weatherApi.js
@@ -0,0 +1,13 @@
+const apiKey = process.env.REACT_APP_API_KEY;
+
+export const fetchWeather = async (city) => {
+    try {
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+        );
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+};
